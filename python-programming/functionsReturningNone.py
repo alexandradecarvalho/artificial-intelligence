dc90d65ea37753e5c9eb9cfb8292cecac4da529c
Alexandra <alexandracarvalho@ua.pt>
2021-02-10 20:07:55 +0000

    Completed Ex 3

--- python-programming/functionsReturningNone.py
+++ python-programming/functionsReturningNone.py
@@ -37,7 +37,7 @@ def lowest(l):
 
 #5 - Recursive function that, given a list of numbers, returns a tuple of the lowest element and a list of all the other elements 
 def popLowest(l):
-	if len(l) > 1:
+	if l:
 		minor_node = l[0] if l[0] < l[-1] else l[-1]
 		next_search_list = l[:-1] if l[0] < l[-1] else l[1:]
 		without_lowest_list = l[1:] if l[0] < l[-1] else l[:-1]
@@ -52,13 +52,45 @@ def popLowest(l):
 		return None
 
 #6 - Recursive function that, given a list of numbers, returns a tuple of the largest and lowest elements 
-def highLow(l1, l2):
-    pass
+def highLow(l):
+	if len(l) > 1:
+		recursive_answer = highLow(l[1:-1])
+		minor = l[0] if l[0] < l[-1] else l[-1]
+		maxim = l[0] if l[0] > l[-1] else l[-1]
+		
+		if recursive_answer:
+			return (recursive_answer[0] if recursive_answer[0] > maxim else maxim, recursive_answer[1] if recursive_answer[1] < minor else minor)	
+		else:
+			return (maxim, minor)
+	else:
+		return None
+
 
 #7 - Recursive function that, given a list of numbers, returns a triple of the largest and lowest elements and a list of all the other elements 
 def popHighLow(l):
-	pass
+	if len(l) > 1:
+		recursive_answer = popHighLow(l[1:-1])
+		minor = l[0] if l[0] < l[-1] else l[-1]
+		maxim = l[0] if l[0] > l[-1] else l[-1]
+		
+		if recursive_answer:
+			minor = minor if minor < recursive_answer[1] else recursive_answer[1]
+			maxim = maxim if maxim > recursive_answer[0] else recursive_answer[0]
+			return (maxim, minor, [x for x in l if x != maxim and x != minor] + recursive_answer[2])	
+		else:
+			return (maxim, minor, [])
+	else:
+		return None
 
 #8 - Recursive function that, given an ordered list of numbers, returns, if possible, a tuple of its mean and median
 def meanMedian(l):
-	pass 
+	if len(l) > 1: 
+		recursive_answer = meanMedian(l[1:-1])
+		if recursive_answer:
+			return (recursive_answer[0] + ((l[0] + l[-1] - recursive_answer[0]) / len(l)), recursive_answer[1])
+		else:
+			return ((l[0] + l[-1])/2, (l[0] + l[1]) / 2)
+	elif len(l) == 1:
+		return (l[0], l[0])
+	else:
+		return None
