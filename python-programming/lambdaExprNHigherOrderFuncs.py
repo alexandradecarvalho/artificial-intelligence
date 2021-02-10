d0b06f1a5e5e80842d300e71936e94250a0ae333
Alexandra <alexandracarvalho@ua.pt>
2021-02-10 22:36:15 +0000

    Ex 4.2 - 4.4

--- python-programming/lambdaExprNHigherOrderFuncs.py
+++ python-programming/lambdaExprNHigherOrderFuncs.py
@@ -1,11 +1,16 @@
+import math 
+
 #1 - Lambda expression that, given an integer n, returns True if it's odd and False otherwise
 isOdd = lambda n : n % 2 == 0
 
 #2 - Lambda expression that, given a number n, returns True if it's positive and False otherwise
+isPositive = lambda n : abs(n) == n
 
 #3 - Lambda expression that, given two numbers x and y, returns True if |x| < |y| and False otherwise
+isLower = lambda x,y : abs(x) < abs(y)
 
 #4 - Lambda expression that, given a pair (x,y), representing Cartesian coordinates of a point, returns a pair (r, θ), corresponding to the polar coordinates of that point
+polarCoordinates = lambda pair : None if pair[0] == 0 else (math.sqrt(pow(pair[0],2) + pow(pair[1],2)), math.atan(pair[1]/pair[0]))
 
 #5 - Function that, given 3 functions of two arguments each, f, g, and h, returns the function with 3 arguments x, y and z = h(f(x,y), g(y,z))
 
@@ -33,3 +38,6 @@ isOdd = lambda n : n % 2 == 0
 
 if __name__ == "__main__":
     print("isOdd ? [200:" + str(isOdd(200)) + ", 353:" + str(isOdd(353)) + ", 0:" + str(isOdd(0)) + ", -5:" + str(isOdd(-5)) + "]")
+    print("isPositive ? [200:" + str(isPositive(200)) + ", 353:" + str(isPositive(353)) + ", 0:" + str(isPositive(0)) + ", -5:" + str(isPositive(-5)) + "]")
+    print("isLower ? [|200| < |353|:" + str(isLower(200,353)) + ", |353| < |0|:" + str(isLower(353,0)) + ", |0| < |-5|:" + str(isLower(0, -5)) + ", |-5|<|3|:" + str(isLower(-5,3)) + "]")
+    print("polarCoordinates ? [(200,353):" + str(polarCoordinates((200,353))) + ", (353,0):" + str(polarCoordinates((353,0))) + ", (0,-5):" + str(polarCoordinates((0, -5))) + ", (-5,3):" + str(polarCoordinates((-5,3))) + "]")
