e487c4f1cf46de60529d960336c2e0db043a16b9
Alexandra <alexandracarvalho@ua.pt>
2021-02-11 00:38:42 +0000

    Ex 4.6-4.10

--- python-programming/lambdaExprNHigherOrderFuncs.py
+++ python-programming/lambdaExprNHigherOrderFuncs.py
@@ -17,14 +17,51 @@ def compoundFunction(f,g,h,x,y,z):
     return h(f(x,y), g(y,z))
 
 #6 - Function that, given a list and a unary boolean function, returns True if the function is positive for all elements in the list and False otherwise
+def areAll(l, funct):
+    if l:
+        return funct(l[0]) and areAll(l[1:], funct)
+    return True
 
 #7 - Function that, given a list and a unary boolean function, returns True if the function is positive for at least one element in the list and False otherwise
+def isOne(l, funct):
+    for element in l:
+        if funct(element):
+            return True
+    return False
 
 #8 - Function that, given two lists, returns True if all elements in the first list are also in the second one, and False otherwise
+def subgroup(l1,l2):
+    if l1:
+        return l1[0] in l2 and subgroup(l1[1:], l2)
+    return True
 
 #9 - Function that, given a not empty list and an order relationship, returns the lowest element on the list according to that order
+def lowest(l, relationship):
+    if len(l) > 1:
+        if relationship(l[0],l[-1]):
+            return lowest(l[:-1], relationship)
+        else:
+            return lowest(l[1:], relationship)
+    elif len(l) == 1:
+        return l[0]
+    else:
+        return None
 
 #10 - Function that, given a not empty list and an order relationship, returns a tuple with the lowest element on the list according to that order and a list with all the other elements
+def popLowest(l, relationship):
+	if l:
+		minor_node = l[0] if relationship(l[0],l[-1]) else l[-1]
+		next_search_list = l[:-1] if relationship(l[0],l[-1]) else l[1:]
+		without_lowest_list = l[1:] if relationship(l[0],l[-1]) else l[:-1]
+
+		recursive_answer = popLowest(next_search_list, relationship)
+		if recursive_answer == None:
+			return (minor_node, without_lowest_list)
+		else:
+			recursive_mini = recursive_answer[0]
+			return (minor_node, without_lowest_list) if not relationship(recursive_mini, minor_node) else (recursive_mini, [x for x in l if x != recursive_mini])
+	else:
+		return None
 
 #11 - Function that, given a list with at least 2 elements and an order relationship, returns a triple with the two lowest elements in the list according to that order and a list with all the other elements
 
@@ -49,4 +86,11 @@ if __name__ == "__main__":
     arePositive = lambda x, y : True if x >= 0 and y >= 0 else False
     print("5 - compoundFunction ? [bothTrue(arePositive(200,353), isLower(353,0)):" + str(compoundFunction(arePositive, isLower, bothTrue, 200,353,0)) + ", isLower(bothTrue(353,0), arePositive(0,-5)):" + str(compoundFunction(bothTrue, arePositive, isLower, 353,0, -5)) + ", arePositive(isLower(0, -5), bothTrue(-5,3)):" + str(compoundFunction(isLower, bothTrue, arePositive, 0, -5, 3)) + "]")
 
+    print("6 - areAll ? [isOdd([200,353,0]):" + str(areAll([200,353,0], isOdd)) + ", isPositive([353,0,-5]):" + str(areAll([353,0,-5], isPositive)) + ", isPositive([0,3,200]):" + str(areAll([0,3,200], isPositive)) + "]")
+    print("7 - isOne ? [isOdd([200,353,0]):" + str(isOne([200,353,0], isOdd)) + ", isPositive([353,0,-5]):" + str(isOne([353,0,-5], isPositive)) + ", isOdd([0,3,200]):" + str(isOne([1,3,-5], isOdd)) + "]")
+    print("8 - subgroup ? [[200,353,0],[-5,1,3]:" + str(subgroup([200,353,0], [-5,1,3])) + ", [353,0,-5],[200,0,353]:" + str(subgroup([353,0,-5], [200,0,353])) + ", [0,3,200],[3,353,35]:" + str(subgroup([0,3,200], [3,353,35])) + ", [3,200],[3,353,35,200]:" + str(subgroup([3,200], [3,353,35,200])) + "]")
     
+    absLT = lambda x, y : True if abs(x) < abs(y) else False
+    print("9 - lowest ? [absLT([200,353,0,-5,3]):" + str(lowest([200,353,0,-5,3], absLT)) + "]")
+    
+    print("10 - popLowest ? [absLT([200,353,0,-5,3]):" + str(popLowest([200,353,0,-5,3], absLT)) + "]")
